032fbcea8551d35ca01e38cf99572535ceb0b94e
Klisboa <eucarolisboa@gmail.com>
2022-01-16 15:48:08 -0300

    First request

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,13 @@
+
+m
+=src/main/java/com/devsuperior/dsmovie/DsmovieApplication.java,c\8\c87388e81731e5a2c8999020c47160e209edf51e
+g
+7src/main/java/com/devsuperior/dsmovie/dto/MovieDTO.java,c\d\cd3941832014694e99c98a5c13b115dae9cb8e92
+g
+7src/main/java/com/devsuperior/dsmovie/dto/ScoreDTO.java,c\d\cdd546c5576819290eea7d00470d7c587cf71c01
+p
+@src/main/java/com/devsuperior/dsmovie/config/SecurityConfig.java,3\7\3750b56bbff0c43d48b032557cf289e4ac32fe78
+v
+Fsrc/main/java/com/devsuperior/dsmovie/controllers/MovieController.java,7\1\715c27382b97ee1cc0fd06a96d86ea510f2e3333
+v
+Fsrc/main/java/com/devsuperior/dsmovie/controllers/ScoreController.java,8\f\8fe5884452d0984462c24c4b1705bc91cd1a36d5
--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,33 +1,43 @@
+import axios from "axios";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
+import { BASE_URL } from "utils/requests";
 
-function Listing () {
+function Listing() {
+
+  //forma errada
+  axios.get(`${BASE_URL}/movie?size=12&page=0`)
+    .then(response => {
+      console.log(response.data);
+    });
+
+
 
   return (
     <>
-    <Pagination />
-    
-    <div className="container">
-      <div className="row">
-      <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-        <MovieCard />
+      <Pagination />
+
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+        </div>
       </div>
-      <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-        <MovieCard />
-      </div>
-      <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-        <MovieCard />
-      </div>
-      <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-        <MovieCard />
-      </div>
-      <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-        <MovieCard />
-      </div>
-    </div>
-    </div>
-    
-    
+
+
     </>
   );
 }
