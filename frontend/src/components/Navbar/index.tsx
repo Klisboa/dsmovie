f890e2b0e14ec1ee8d569c05c19d72f5694f866d
Klisboa <eucarolisboa@gmail.com>
2022-01-16 19:30:21 -0300

    alterando navbar: redirecionar para o meu github pessoal

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,17 +1,17 @@
-import{ReactComponent as GithubIcon} from 'assets/img/github.svg';
+import { ReactComponent as GithubIcon } from 'assets/img/github.svg';
 import './styles.css';
 
 function Navibar() {
 
   return (
-        <header>
-          <nav className="container">
-            <div className="dsmovie-nav-content">
-              <h1>DSMovie</h1>
-              <a href="https://github.com/Klisboa">
-               <div className="dsmovie-contact-container">
-              <GithubIcon/>
-              <p className="dsmovie-contact-link">/devsuperior</p>
+    <header>
+      <nav className="container">
+        <div className="dsmovie-nav-content">
+          <h1>DSMovie</h1>
+          <a href="https://github.com/Klisboa">
+            <div className="dsmovie-contact-container">
+              <GithubIcon />
+              <p className="dsmovie-contact-link">/Klisboa</p>
             </div>
 
           </a>
